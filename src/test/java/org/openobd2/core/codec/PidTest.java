f78b82e20535902536ba81bf65bd839682181c52
Tomasz.Zebrowski <zebrowski.tomek@gmail.com>
2021-01-01 12:28:01 +0100

    Enhance codec lookup in PidTest

--- src/test/java/org/openobd2/core/codec/PidTest.java
+++ src/test/java/org/openobd2/core/codec/PidTest.java
@@ -5,13 +5,23 @@ import java.io.InputStream;
 import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
-import org.openobd2.core.codec.Codec;
-import org.openobd2.core.codec.CodecRegistry;
 import org.openobd2.core.command.obd.ObdCommand;
 import org.openobd2.core.pid.PidDefinition;
 import org.openobd2.core.pid.PidRegistry;
 
 public interface PidTest {
+
+	static final class CodecFinder {
+
+		static Optional<Codec<?>> find(final String mode, final String pid, final InputStream source) {
+			final PidRegistry pidRegistry = PidRegistry.builder().source(source).build();
+
+			final CodecRegistry codecRegistry = CodecRegistry.builder().pids(pidRegistry).build();
+			final PidDefinition pidDef = pidRegistry.findBy(mode, pid);
+			Assertions.assertThat(pidDef).isNotNull();
+			return codecRegistry.findCodec(new ObdCommand(pidDef));
+		}
+	}
 
 	default void mode01Test(String rawData, Object expectedValue) throws IOException {
 
@@ -21,13 +31,8 @@ public interface PidTest {
 		try (final InputStream source = Thread.currentThread().getContextClassLoader()
 				.getResourceAsStream("generic.json")) {
 
-			final PidRegistry pidRegistry = PidRegistry.builder().source(source).build();
+			final Optional<Codec<?>> codec = CodecFinder.find(mode, pid, source);
 
-			final CodecRegistry codecRegistry = CodecRegistry.builder().pids(pidRegistry).build();
-			final PidDefinition pidDef = pidRegistry.findBy(mode, pid);
-			Assertions.assertThat(pidDef).isNotNull();
-
-			final Optional<Codec<?>> codec = codecRegistry.findCodec(new ObdCommand(pidDef));
 			if (codec.isPresent()) {
 				final Object value = codec.get().decode(rawData);
 				Assertions.assertThat(value).isEqualTo(expectedValue);
@@ -45,13 +50,8 @@ public interface PidTest {
 		try (final InputStream source = Thread.currentThread().getContextClassLoader()
 				.getResourceAsStream("alfa.json")) {
 
-			final PidRegistry pidRegistry = PidRegistry.builder().source(source).build();
+			final Optional<Codec<?>> codec = CodecFinder.find(mode, pid, source);
 
-			final CodecRegistry codecRegistry = CodecRegistry.builder().pids(pidRegistry).build();
-			final PidDefinition pidDef = pidRegistry.findBy(mode, pid);
-			Assertions.assertThat(pidDef).isNotNull();
-
-			final Optional<Codec<?>> codec = codecRegistry.findCodec(new ObdCommand(pidDef));
 			if (codec.isPresent()) {
 				final Object value = codec.get().decode(rawData);
 				Assertions.assertThat(value).isEqualTo(expectedValue);
