9ba5700eed85dcb0140ff7cda9aa1213f1cdb8b1
Tomasz.Zebrowski <zebrowski.tomek@gmail.com>
2021-01-03 12:57:15 +0100

    Make CommandReplySubscriber public

--- src/main/java/org/openobd2/core/CommandReplySubscriber.java
+++ src/main/java/org/openobd2/core/CommandReplySubscriber.java
@@ -2,12 +2,9 @@ package org.openobd2.core;
 
 import org.openobd2.core.command.CommandReply;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import rx.Observer;
 
-@NoArgsConstructor(access = AccessLevel.PACKAGE)
-abstract class CommandReplySubscriber implements Observer<CommandReply<?>> {
+public abstract class CommandReplySubscriber implements Observer<CommandReply<?>> {
 	@Override
 	public void onCompleted() {
 	}
