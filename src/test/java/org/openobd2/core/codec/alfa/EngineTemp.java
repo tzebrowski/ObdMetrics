234a5f7c1d420184fbb6e5100cb62535ce705bb3
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2021-01-19 11:31:55 +0100

    Improve mode 22 engine coolant formula

--- src/test/java/org/openobd2/core/codec/alfa/EngineTemp.java
+++ src/test/java/org/openobd2/core/codec/alfa/EngineTemp.java
@@ -8,17 +8,17 @@ import org.openobd2.core.codec.PidTest;
 public class EngineTemp implements PidTest {
 	@Test
 	public void t1() throws IOException {
-		mode22Test("62100340", 0.0);
+		mode22Test("62100340", -1);
 	}
 
 	@Test
 	public void t2() throws IOException {
-		mode22Test("621003AB", 80.25);
+		mode22Test("621003AB", 80);
 	}
 
 	@Test
 	public void t3() throws IOException {
-		mode22Test("621003AA", 79.50);
+		mode22Test("621003AA", 79);
 	}
 
 	@Test
