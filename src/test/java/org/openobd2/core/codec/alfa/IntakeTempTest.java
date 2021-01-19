90774b16566bbec0684c16730df57c32b1aa4fbb
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2021-01-19 12:23:21 +0100

    Improve mode 22  intake temperature formula

--- src/test/java/org/openobd2/core/codec/alfa/IntakeTempTest.java
+++ src/test/java/org/openobd2/core/codec/alfa/IntakeTempTest.java
@@ -7,8 +7,13 @@ import org.openobd2.core.codec.PidTest;
 
 public class IntakeTempTest  implements PidTest{
 	@Test
-	public void tempTest() throws IOException {
-		mode22Test("62193540",0.0);
-		mode22Test("62193542",2.0);
+	public void t0() throws IOException {
+		mode22Test("62193540",-1);//0
 	}
+	
+	@Test
+	public void t1() throws IOException {
+		mode22Test("62193542",1);//2.0
+	}
+	
 }
