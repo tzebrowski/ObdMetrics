0315bac446ba314fbad10a790496d50122755aae
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2021-03-07 20:13:45 +0100

    Enhance conditional sleep juni-tests

--- src/test/java/org/obd/metrics/api/ConditionalSleepTest.java
+++ src/test/java/org/obd/metrics/api/ConditionalSleepTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class ConditionalSleepTest {
 	
 	@Test
-	void sleepTest() throws InterruptedException {
+	void equalToSleepTimeCondition() throws InterruptedException {
 		var conditionalSleep = ConditionalSleep
 		        .builder()
 		        .sleepTime(20l)
@@ -20,7 +20,7 @@ public class ConditionalSleepTest {
 	}
 
 	@Test
-	void sleep2Test() throws InterruptedException {
+	void greaterThanSleepTimeCondition() throws InterruptedException {
 
 		var conditionalSleep = ConditionalSleep
 		        .builder()
@@ -34,5 +34,22 @@ public class ConditionalSleepTest {
 
 		tt = System.currentTimeMillis() - tt;
 		Assertions.assertThat(tt).isGreaterThanOrEqualTo(sleepTime);
+	}
+	
+	@Test
+	void conditionTest() throws InterruptedException {
+
+		var conditionalSleep = ConditionalSleep
+		        .builder()
+		        .sleepTime(5l)
+		        .condition(() -> true)
+		        .build();
+
+		int sleepTime = 170;
+		long tt = System.currentTimeMillis();
+		conditionalSleep.sleep(sleepTime);
+
+		tt = System.currentTimeMillis() - tt;
+		Assertions.assertThat(tt).isLessThan(50);
 	}
 }
