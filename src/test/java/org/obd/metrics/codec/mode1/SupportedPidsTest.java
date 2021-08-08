dd44eb0808d0e946fec9dea5ff342bea198bab4e
Tomek Zebrowski <tomasz.zebrowski@globallogic.com>
2021-08-08 20:20:06 +0200

    Enhance SupportedPidsTest junit tests

--- src/test/java/org/obd/metrics/codec/mode1/SupportedPidsTest.java
+++ src/test/java/org/obd/metrics/codec/mode1/SupportedPidsTest.java
@@ -1,66 +1,40 @@
 package org.obd.metrics.codec.mode1;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.obd.metrics.codec.Codec;
-import org.obd.metrics.codec.CodecRegistry;
 import org.obd.metrics.command.obd.SupportedPidsCommand;
 
-@SuppressWarnings("unchecked")
 public class SupportedPidsTest {
-	
-	
+
 	@Test
-	public void noPidsTest() throws IOException {
+	public void invalidDataTest() {
 
-		try (final InputStream source = Thread.currentThread().getContextClassLoader()
-		        .getResourceAsStream("mode01.json")) {
-
-			final String rawData = "2100BE3E2F00";
-			final CodecRegistry codecRegistry = CodecRegistry.builder().equationEngine("JavaScript").build();
-			final SupportedPidsCommand command = new SupportedPidsCommand("00");
-			final Codec<?> codec = codecRegistry.findCodec(command).get();
-			final List<String> supportedPids = (List<String>) codec.decode(command.getPid(), rawData);
-			Assertions.assertThat(supportedPids).isNotNull().isEmpty();
-
-		}
-	}
-	
-	@Test
-	public void pids00() throws IOException {
-
-		try (final InputStream source = Thread.currentThread().getContextClassLoader()
-		        .getResourceAsStream("mode01.json")) {
-
-			final String rawData = "4100BE3E2F00";
-			final CodecRegistry codecRegistry = CodecRegistry.builder().equationEngine("JavaScript").build();
-			final SupportedPidsCommand command = new SupportedPidsCommand("00");
-			final Codec<?> codec = codecRegistry.findCodec(command).get();
-			final List<String> supportedPids = (List<String>) codec.decode(command.getPid(), rawData);
-
-			Assertions.assertThat(supportedPids).isNotNull().isNotEmpty().containsExactly("01", "03", "04", "05", "06",
-			        "07", "0b", "0c", "0d", "0e", "0f", "13", "15", "16", "17", "18");
-
-		}
+		final String rawData = "2100BE3E2F00";
+		final SupportedPidsCommand codec = new SupportedPidsCommand("00");
+		final List<String> result = codec.decode(codec.getPid(), rawData);
+		Assertions.assertThat(result).isNotNull().isEmpty();
 	}
 
 	@Test
-	public void pids20() throws IOException {
+	public void pids00() {
 
-		try (final InputStream source = Thread.currentThread().getContextClassLoader()
-		        .getResourceAsStream("mode01.json")) {
+		final String rawData = "4100BE3E2F00";
+		final SupportedPidsCommand codec = new SupportedPidsCommand("00");
+		final List<String> result = codec.decode(codec.getPid(), rawData);
 
-			final String rawData = "4120a0001000";
-			final CodecRegistry codecRegistry = CodecRegistry.builder().equationEngine("JavaScript").build();
+		Assertions.assertThat(result).isNotNull().isNotEmpty().containsExactly("01", "03", "04", "05", "06",
+		        "07", "0b", "0c", "0d", "0e", "0f", "13", "15", "16", "17", "18");
 
-			final SupportedPidsCommand command = new SupportedPidsCommand("20");
-			final Codec<?> codec = codecRegistry.findCodec(command).get();
-			final List<String> supportedPids = (List<String>) codec.decode(command.getPid(), rawData);
-			Assertions.assertThat(supportedPids).isNotNull().isNotEmpty().containsExactly("01", "03", "14");
-		}
+	}
+
+	@Test
+	public void pids20() {
+
+		final String rawData = "4120a0001000";
+		final SupportedPidsCommand codec = new SupportedPidsCommand("20");
+		final List<String> result = codec.decode(codec.getPid(), rawData);
+		Assertions.assertThat(result).isNotNull().isNotEmpty().containsExactly("01", "03", "14");
 	}
 }
