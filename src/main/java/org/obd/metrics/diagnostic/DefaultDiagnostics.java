a37b0b4c9a174c508a43bdddcdcb94ee660b5a26
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2022-01-31 17:20:46 +0100

    Decouple logic of Diagnosis component into 2 sub-components.
    Decouple logic of histogram building and rate collecting into two
    separate components. Introduce HistogramBuilder and RateCollector
    classes that have high specialization.

--- src/main/java/org/obd/metrics/diagnostic/DefaultDiagnostics.java
+++ src/main/java/org/obd/metrics/diagnostic/DefaultDiagnostics.java
@@ -1,123 +1,44 @@
 package org.obd.metrics.diagnostic;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
-import java.util.SortedMap;
 
 import org.obd.metrics.ObdMetric;
 import org.obd.metrics.ReplyObserver;
 import org.obd.metrics.pid.PidDefinition;
 
-import com.codahale.metrics.Meter;
-import com.codahale.metrics.MetricRegistry;
-import com.dynatrace.dynahist.layout.Layout;
-import com.dynatrace.dynahist.layout.LogQuadraticLayout;
-
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 final class DefaultDiagnostics extends ReplyObserver<ObdMetric> implements Diagnostics {
 
-	private MetricRegistry metrics = new MetricRegistry();
-	private final Map<String, PidDefinition> pidsMapping = new HashMap<String, PidDefinition>();
-	private final Map<String, com.dynatrace.dynahist.Histogram> hists = new HashMap<String, com.dynatrace.dynahist.Histogram>();
-
+	private final RateCollector rateCollector = new RateCollector();
+	private final HistogramBuilder histogramCollector = new HistogramBuilder();
+	
 	@Override
 	public void onNext(ObdMetric obdMetric) {
-		try {
-			final PidDefinition pidDefinition = obdMetric.getCommand().getPid();
-			log.trace("Update histogram: {} {}", pidDefinition.getPid(), obdMetric.valueToDouble());
-			findMeterBy(pidDefinition).mark();
-			pidsMapping.put(getMeterKey(pidDefinition), pidDefinition);
-			findHistogram(pidDefinition).addValue(obdMetric.valueToDouble());
-		} catch (Throwable e) {
-			log.debug("Failed to update histogram", e);
-		}
+		rateCollector.update(obdMetric);
+		histogramCollector.update(obdMetric);
 	}
 
 	@Override
 	public void reset() {
-		metrics = new MetricRegistry();
-		pidsMapping.clear();
-		hists.clear();
+		rateCollector.reset();
+		histogramCollector.reset();
 	}
 
 	@Override
 	public Optional<Rate> getRateBy(RateType rateType) {
-		if (metrics.getMeters().isEmpty()) {
-			return Optional.empty();
-		} else {
-			return pidsMapping.entrySet()
-			        .stream()
-			        .filter(p -> p.getValue().getPriority() == 0)
-			        .map(p -> getRate(rateType, p.getKey(), metrics.getMeters().get(p.getKey())))
-			        .findFirst()
-			        .orElse(firstKeyRate(rateType));
-		}
+		return rateCollector.getRateBy(rateType);
 	}
 
 	@Override
 	public Histogram findHistogramBy(PidDefinition pid) {
-		return new DefaultHistogram(findHistogram(pid));
+		return histogramCollector.findHistogramBy(pid);
 	}
 
 	@Override
 	public Optional<Rate> getRateBy(RateType rateType, PidDefinition pid) {
-		final String key = getMeterKey(pid);
-		final Meter meter = findMeterBy(pid);
-		return getRate(rateType, key, meter);
-	}
-
-	private Meter findMeterBy(PidDefinition pid) {
-		return metrics.meter(getMeterKey(pid));
-	}
-
-	private String getMeterKey(PidDefinition pid) {
-		return "meter." + pid.getId();
-	}
-
-	private com.dynatrace.dynahist.Histogram findHistogram(PidDefinition pid) {
-		final String key = "hist." + pid.getId();
-		if (hists.containsKey(key)) {
-			return hists.get(key);
-		} else {
-			Layout layout = LogQuadraticLayout.create(1e-5, 1e-2, -1e9, 1e9);
-
-			final com.dynatrace.dynahist.Histogram histogram = com.dynatrace.dynahist.Histogram
-			        .createDynamic(layout);
-			hists.put(key, histogram);
-			return histogram;
-		}
-	}
-
-	private Optional<Rate> getRate(final String key, final double rate, final RateType rateType) {
-		if (log.isTraceEnabled()) {
-			log.trace("Key: {}, rate: {}", key, rate);
-		}
-
-		return Optional.of(new Rate(rateType, rate, key));
-	}
-
-	private Optional<Rate> getRate(RateType rateType, final String key, final Meter meter) {
-		switch (rateType) {
-		default:
-			return getRate(key, meter.getMeanRate(), rateType);
-		case ONE_MINUTE:
-			return getRate(key, meter.getOneMinuteRate(), rateType);
-		case FIVE_MINUTES:
-			return getRate(key, meter.getFiveMinuteRate(), rateType);
-		case MEAN:
-			return getRate(key, meter.getMeanRate(), rateType);
-		}
-	}
-
-	private Optional<Rate> firstKeyRate(RateType rateType) {
-		final SortedMap<String, Meter> meters = metrics.getMeters();
-		final String key = meters.firstKey();
-		return getRate(key, meters.get(key).getMeanRate(), rateType);
+		return rateCollector.getRateBy(rateType, pid);
 	}
 }
--- src/main/java/org/obd/metrics/diagnostic/Diagnostics.java
+++ src/main/java/org/obd/metrics/diagnostic/Diagnostics.java
@@ -4,8 +4,14 @@ import java.util.Optional;
 
 import org.obd.metrics.pid.PidDefinition;
 
+/**
+ * Facade interface that provide diagnosis information about current state of
+ * data collection.
+ * 
+ * @author tomasz.zebrowski
+ */
 public interface Diagnostics {
-	
+
 	Histogram findHistogramBy(PidDefinition pid);
 
 	Optional<Rate> getRateBy(RateType rateType, PidDefinition pid);
