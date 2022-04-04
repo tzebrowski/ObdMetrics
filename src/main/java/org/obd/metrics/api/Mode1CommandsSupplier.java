5c3471d0e2277b305075d2cfc450f27e1df29837
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2022-04-04 09:18:04 +0200

    Collect only unique PID's
    Ensure that only unique PID's are result of mode1 command supplier

--- src/main/java/org/obd/metrics/api/Mode1CommandsSupplier.java
+++ src/main/java/org/obd/metrics/api/Mode1CommandsSupplier.java
@@ -2,11 +2,15 @@ package org.obd.metrics.api;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.obd.metrics.codec.batch.BatchCodec;
+import org.obd.metrics.command.obd.BatchObdCommand;
 import org.obd.metrics.command.obd.ObdCommand;
 import org.obd.metrics.pid.PidDefinition;
 import org.obd.metrics.pid.PidDefinition.CommandType;
@@ -40,9 +44,12 @@ final class Mode1CommandsSupplier extends CommandsSuplier {
 			final List<ObdCommand> obdCommands = commands
 			        .stream()
 			        .filter(p -> CommandType.OBD.equals(p.getPid().getCommandType()))
+			        .filter(distinctByKey(c -> c.getPid().getPid()))
 			        .collect(Collectors.toList());
 
-			result.addAll(BatchCodec.instance(null, obdCommands).encode());
+			List<BatchObdCommand> encode = BatchCodec.instance(null, new ArrayList<>(obdCommands)).encode();
+			
+			result.addAll(encode);
 			// add at the end commands that does not support batch fetching
 			result.addAll(commands.stream().filter(p -> !CommandType.OBD.equals(p.getPid().getCommandType()))
 			        .collect(Collectors.toList()));
@@ -54,6 +61,13 @@ final class Mode1CommandsSupplier extends CommandsSuplier {
 		return result;
 	}
 
+	private <T> Predicate<T> distinctByKey(
+	        Function<? super T, ?> keyExtractor) {
+
+		final Map<Object, Boolean> seen = new ConcurrentHashMap<>();
+		return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+	}
+	
 	private Function<? super Long, ? extends ObdCommand> idToPid() {
 		return pid -> {
 			final PidDefinition findBy = pidRegistry.findBy(pid);
