110a29149f8895934cb0323ad478b8b50097712b
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2022-07-23 10:00:02 +0200

    feat: Alfa PID's upgrade
    * Mark "Ignition Angle Reduction" PIDs as unstable
    * Add atmospheric pressure PID
    * Add fuel consumption PID
    * Upgrade battery calculation PID

--- src/test/java/org/obd/metrics/codec/giulietta_qv_med17_3_1/FuelConsumptionTest.java
+++ src/test/java/org/obd/metrics/codec/giulietta_qv_med17_3_1/FuelConsumptionTest.java
@@ -3,14 +3,14 @@ package org.obd.metrics.codec.giulietta_qv_med17_3_1;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class BatterVoltageTest implements Giulietta_QV_Med_17_3_1_Test {
+public class FuelConsumptionTest implements Giulietta_QV_Med_17_3_1_Test {
 
 	@ParameterizedTest
 	@CsvSource(value = { 
-			"62100496=14.15", 
-			"62100482=12.26",
-			"62100484=12.45" }, delimiter = '=')
+			"6219420000=0",
+	}, delimiter = '=')
 	public void parameterizedTest(String input, String expected) {
 		assertEquals(input, Double.parseDouble(expected));
 	}
+
 }
--- src/test/java/org/obd/metrics/codec/giulietta_qv_med17_3_1/OilTempTest.java
+++ src/test/java/org/obd/metrics/codec/giulietta_qv_med17_3_1/OilTempTest.java
@@ -8,18 +8,19 @@ public class OilTempTest implements Giulietta_QV_Med_17_3_1_Test {
 	
 	@ParameterizedTest
 	@CsvSource(value = {
-			"62194F2D85=0.0",
-			"62194F2DC5=1.0",
-			"62194F2DA5=1.0",
+			"62194F2D85=0",
+			"62194F2DC5=1",
+			"62194F2DA5=1",
 			"62194F2E25=3",
 	        "62194F2E45=4",
 			"62194F2FA5=11",
+			"62194F30E5=20",
 			"62194F3725=57",
 			"62194F3745=58",
 			"62194F3CE5=92",
 			"62194F3BC5=86",
 			"62194F3B85=85",
-	        "62194F3E65=99.0",
+	        "62194F3E65=99",
 	         }, delimiter = '=')
 	public void parameterizedTest(String input, String expected) {
 		assertCloseTo(input, Float.parseFloat(expected),3);
