a78cd5a1f6ba2de351aed5f58ca0bbdfaa440a3b
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2022-08-06 21:18:07 +0200

    feat: Enable throttle angle junit-test

--- src/test/java/org/obd/metrics/codec/giulietta_qv_med17_3_1/ThrottleAngleTest.java
+++ src/test/java/org/obd/metrics/codec/giulietta_qv_med17_3_1/ThrottleAngleTest.java
@@ -1,10 +1,8 @@
 package org.obd.metrics.codec.giulietta_qv_med17_3_1;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-@Disabled
 public class ThrottleAngleTest implements Giulietta_QV_Med_17_3_1_Test {
 	@ParameterizedTest
 	@CsvSource(value = { 
@@ -12,6 +10,6 @@ public class ThrottleAngleTest implements Giulietta_QV_Med_17_3_1_Test {
 	        "621862010C=6.643",
 			}, delimiter = '=')
 	public void parameterizedTest(String input, String expected) {
-		assertEquals(input, Double.parseDouble(expected));
+		assertCloseTo(input, Float.parseFloat(expected), 1.0f);
 	}
 }
