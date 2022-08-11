00ac56c191655bba8d1b9620efe02b0083defb1e
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2022-08-11 13:53:18 +0200

    fix: Update formula for Oil degradation.

--- src/test/java/org/obd/metrics/codec/giulia_2_0_gme/OilDegradationTest.java
+++ src/test/java/org/obd/metrics/codec/giulia_2_0_gme/OilDegradationTest.java
@@ -1,15 +1,13 @@
 package org.obd.metrics.codec.giulia_2_0_gme;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-@Disabled
 public class OilDegradationTest implements Giulia_2_0_GME_Test {
 
 	@ParameterizedTest
 	@CsvSource(value = { 
-			"6238137D70=49",
+			"6238137D70=49.0",
 			}, delimiter = '=')
 	public void parameterizedTest(String input, String expected) {
 		assertEquals(input, Double.parseDouble(expected));
