b52ea4e712b7e325e5ecdbbe409b4bb5b6e181b2
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2022-08-12 09:53:40 +0200

    feat: Giulietta - Add Oil degradation level PID

--- src/test/java/org/obd/metrics/codec/giulia_2_0_gme/EGTTest.java
+++ src/test/java/org/obd/metrics/codec/giulia_2_0_gme/EGTTest.java
@@ -1,6 +1,5 @@
 package org.obd.metrics.codec.giulia_2_0_gme;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
---
+++ src/test/java/org/obd/metrics/codec/giulietta_qv_med17_3_1/OilDegradationLevelTest.java
@@ -0,0 +1,16 @@
+package org.obd.metrics.codec.giulietta_qv_med17_3_1;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+public class OilDegradationLevelTest implements Giulietta_QV_Med_17_3_1_Test {
+	
+	@ParameterizedTest
+	@CsvSource(value = {
+			"62381A5E=94.0",
+			 }, delimiter = '=')
+	public void parameterizedTest(String input, String expected) {
+		assertEquals(input, Double.parseDouble(expected));
+	}
+}
