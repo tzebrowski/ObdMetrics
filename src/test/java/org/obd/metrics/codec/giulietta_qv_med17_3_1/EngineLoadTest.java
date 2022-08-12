95a0bed225c4f71d69ed3bf797f0206190e70b57
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2022-08-12 10:04:08 +0200

    feat: Giulietta - Add engine load metric

--- src/test/java/org/obd/metrics/codec/giulietta_qv_med17_3_1/EngineLoadTest.java
+++ src/test/java/org/obd/metrics/codec/giulietta_qv_med17_3_1/EngineLoadTest.java
@@ -1,16 +1,16 @@
 package org.obd.metrics.codec.giulietta_qv_med17_3_1;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-
-public class OilDegradationLevelTest implements Giulietta_QV_Med_17_3_1_Test {
-	
+@Disabled
+public class EngineLoadTest implements Giulietta_QV_Med_17_3_1_Test {
 	@ParameterizedTest
-	@CsvSource(value = {
-		"62381A5E=94.0",
-	}, delimiter = '=')
+	@CsvSource(value = { 
+			"62181D10AB=100",
+			}, delimiter = '=')
 	public void parameterizedTest(String input, String expected) {
-		assertEquals(input, Double.parseDouble(expected));
+		assertCloseTo(input, Float.parseFloat(expected),01.f);
 	}
 }
