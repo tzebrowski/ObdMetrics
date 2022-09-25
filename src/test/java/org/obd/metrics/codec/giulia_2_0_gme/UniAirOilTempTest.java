db9a29996556f2757e12ad5fa97eed0a9d56d96f
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2022-09-25 16:59:54 +0200

    fix: Improve UniAir temp calculation

--- src/test/java/org/obd/metrics/codec/giulia_2_0_gme/UniAirOilTempTest.java
+++ src/test/java/org/obd/metrics/codec/giulia_2_0_gme/UniAirOilTempTest.java
@@ -7,8 +7,8 @@ public class UniAirOilTempTest implements Giulia_2_0_GME_Test {
 
 	@ParameterizedTest
 	@CsvSource(value = { 
-//			"62198E0323=10.19",
-//			"62198E034B=12.69",
+			"62198E0323=10.19",
+			"62198E034B=12.69",
 			"62198E07DB=85.19",
 			"62198E0861=89.0",
 			"62198E0649=67.0"
