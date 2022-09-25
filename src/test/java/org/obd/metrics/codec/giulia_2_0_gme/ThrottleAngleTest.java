ab4df441c07c64f29ffb4bd7284c99cdee4c44b1
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2022-09-25 13:22:43 +0200

    feat: 2_0 GME: Add Throttle angle PID

--- src/test/java/org/obd/metrics/codec/giulia_2_0_gme/ThrottleAngleTest.java
+++ src/test/java/org/obd/metrics/codec/giulia_2_0_gme/ThrottleAngleTest.java
@@ -3,17 +3,13 @@ package org.obd.metrics.codec.giulia_2_0_gme;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class UniAirOilTempTest implements Giulia_2_0_GME_Test {
-
+public class ThrottleAngleTest implements Giulia_2_0_GME_Test {
 	@ParameterizedTest
 	@CsvSource(value = { 
-//			"62198E0323=10.19",
-//			"62198E034B=12.69",
-			"62198E07DB=85.19",
-			"62198E0861=89.0",
-			"62198E0649=67.0"
-			}, delimiter = '=')
+			"6218020080=8.0",	
+			"621802008D=9.6"
+	        }, delimiter = '=')
 	public void parameterizedTest(String input, String expected) {
-		assertCloseTo(input, Float.parseFloat(expected),2.0f);
+		assertCloseTo(input, Float.parseFloat(expected), 0.5f);
 	}
 }
