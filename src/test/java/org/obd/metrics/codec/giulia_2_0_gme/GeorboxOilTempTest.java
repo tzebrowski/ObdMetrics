01b7b0afa96243f1fa52db1e6adeaa7f448d9d94
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2023-05-06 10:39:50 +0200

    feat: Add Gearbox Oil Temp PID to Giulia 2.0 GME

---
+++ src/test/java/org/obd/metrics/codec/giulia_2_0_gme/GeorboxOilTempTest.java
@@ -0,0 +1,16 @@
+package org.obd.metrics.codec.giulia_2_0_gme;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class GeorboxOilTempTest implements Giulia_2_0_GME_Test {
+
+	@ParameterizedTest
+	@CsvSource(value = { 
+			"6204FE4E=38.0",
+			"6204FE54=44.0",
+			}, delimiter = '=')
+	public void parameterizedTest(String input, String expected) {
+		assertEquals(Boolean.TRUE, input, Double.parseDouble(expected));
+	}
+}
