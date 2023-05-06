d6f0dc53cf8849bd38737c2280650f75b57fc51d
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2023-05-06 11:19:34 +0200

    feat: Enable catalyst temp PID

--- src/test/java/org/obd/metrics/codec/giulia_2_0_gme/CatalystTempTest.java
+++ src/test/java/org/obd/metrics/codec/giulia_2_0_gme/CatalystTempTest.java
@@ -1,17 +1,16 @@
 package org.obd.metrics.codec.giulia_2_0_gme;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-@Disabled
 public class CatalystTempTest implements Giulia_2_0_GME_Test {
 	
 	@ParameterizedTest
 	@CsvSource(value = { 
 			"6218375A=400",
+			"62183754=370"
 			}, delimiter = '=')
 	public void parameterizedTest(String input, String expected) {
-		assertEquals(input, Float.parseFloat(expected));
+		assertCloseTo(input, Float.parseFloat(expected),5f);
 	}
 }
