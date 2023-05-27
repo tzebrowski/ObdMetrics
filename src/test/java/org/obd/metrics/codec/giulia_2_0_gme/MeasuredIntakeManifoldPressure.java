5e82392ae026a77341abd0e4af9ffa0608c53360
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2023-05-27 09:55:22 +0200

    feat: Add more junit tests for intake manifold pressure for 2.0 GME

--- src/test/java/org/obd/metrics/codec/giulia_2_0_gme/MeasuredIntakeManifoldPressure.java
+++ src/test/java/org/obd/metrics/codec/giulia_2_0_gme/MeasuredIntakeManifoldPressure.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class MeasuredIntakeManifoldPressure implements Giulia_2_0_GME_Test {
 
-	//13:05:39.352 TRACE DefaultConnector - TX: 22 1937 181F 180E 2
-	//13:05:39.388 TRACE DefaultConnector - RX: 0090:621937011C181:1F0119, processing time: 36ms
-	
 	@ParameterizedTest
 	@CsvSource(value = { 
 			"621937011C=284.0",
+			"62193703E2=994.0"
 			}, delimiter = '=')
 	public void parameterizedTest(String input, String expected) {
 		assertEquals(input, Double.parseDouble(expected));
