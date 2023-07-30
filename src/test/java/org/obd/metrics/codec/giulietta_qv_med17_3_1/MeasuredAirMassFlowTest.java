6cd844df1befac284dbc69fbd93ce899ef894ecf
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2023-07-30 10:28:06 +0200

    feat: Extend tests for measured MAF for 1.75 tbi engines

--- src/test/java/org/obd/metrics/codec/giulietta_qv_med17_3_1/MeasuredAirMassFlowTest.java
+++ src/test/java/org/obd/metrics/codec/giulietta_qv_med17_3_1/MeasuredAirMassFlowTest.java
@@ -10,7 +10,9 @@ public class MeasuredAirMassFlowTest implements Giulietta_QV_Med_17_3_1_Test {
 			"62180E0059=8.9",
 			"62180E0069=10.5", 
 			"62180E015C=34.8",
-			"62180E115C=444.4"}, delimiter = '=')
+			"62180E115C=444.4",
+			"62180E0089=13.7"
+	}, delimiter = '=')
 	public void parameterizedTest(String input, String expected) {
 		assertEquals(6013l,input, Double.parseDouble(expected));
 	}
