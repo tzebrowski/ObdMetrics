c630191e63502ec5fef1a961652917c8d0428f2a
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2023-11-03 14:46:33 +0100

    fix: Fix battery voltage formula for 2.0 GME

--- src/test/java/org/obd/metrics/codec/giulia_2_0_gme/BatteryVoltageTest.java
+++ src/test/java/org/obd/metrics/codec/giulia_2_0_gme/BatteryVoltageTest.java
@@ -21,7 +21,7 @@ package org.obd.metrics.codec.giulia_2_0_gme;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class BatterVoltageTest implements Giulia_2_0_GME_Test {
+public class BatteryVoltageTest implements Giulia_2_0_GME_Test {
 
 	@ParameterizedTest
 	@CsvSource(value = { 
