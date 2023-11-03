34ceeb0cb119ecfe26b6a7cd11b85e0a256269af
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2023-11-03 10:02:26 +0100

    feat: Add target air flow rate for 2.0 GME

--- src/test/java/org/obd/metrics/codec/giulia_2_0_gme/MeasuredAirFlowRateTest.java
+++ src/test/java/org/obd/metrics/codec/giulia_2_0_gme/MeasuredAirFlowRateTest.java
@@ -21,14 +21,14 @@ package org.obd.metrics.codec.giulia_2_0_gme;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class AirFlowRateTest implements Giulia_2_0_GME_Test {
+public class MeasuredAirFlowRateTest implements Giulia_2_0_GME_Test {
 
 	@ParameterizedTest
 	@CsvSource(value = { 
 			"62182F0000=0.0",
-			"62182F0055=10.53",
+			"62182F0055=0.85",
 			}, delimiter = '=')
-	public void parameterizedTest(String input, Float expected) {
-		assertCloseTo(input, expected, 0.5f);
+	public void parameterizedTest(String input, Double expected) {
+		assertEquals(input, expected);
 	}
 }
