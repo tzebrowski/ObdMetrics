fa3174df058be3cab442bf6cc49892f9d02a816b
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2023-11-09 10:57:42 +0100

    feat: Add junits for Target Boost Pressure PID for 2.0 GME

---
+++ src/test/java/org/obd/metrics/codec/giulia_2_0_gme/MeasuredBoostPressureTest.java
@@ -0,0 +1,34 @@
+/** 
+ * Copyright 2019-2023, Tomasz Żebrowski
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+package org.obd.metrics.codec.giulia_2_0_gme;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class MeasuredBoostPressureTest implements Giulia_2_0_GME_Test {
+
+	@ParameterizedTest
+	@CsvSource(value = { 
+			"62195A03EC=1004",
+			"62195A03E9=1001",
+			}, delimiter = '=')
+	public void parameterizedTest(String input, Integer expected) {
+		assertEquals(input, expected);
+	}
+}
--- src/test/java/org/obd/metrics/codec/giulia_2_0_gme/TargetBoostPressureTest.java
+++ src/test/java/org/obd/metrics/codec/giulia_2_0_gme/TargetBoostPressureTest.java
@@ -21,12 +21,12 @@ package org.obd.metrics.codec.giulia_2_0_gme;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class BoostPressureTest implements Giulia_2_0_GME_Test {
+public class TargetBoostPressureTest implements Giulia_2_0_GME_Test {
 
 	@ParameterizedTest
 	@CsvSource(value = { 
-			"62195A03EC=1004",
-			"62195A03E9=1001",
+			"62195903EC=1004",
+			"62195903E9=1001",
 			}, delimiter = '=')
 	public void parameterizedTest(String input, Integer expected) {
 		assertEquals(input, expected);
