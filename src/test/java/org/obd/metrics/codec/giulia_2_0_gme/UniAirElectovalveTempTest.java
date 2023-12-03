f23e89cc16fa77db6eecbcb1827fab84d971e9b8
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2023-12-03 18:10:39 +0100

    feat: Add UniAir electorvalve PID for 2.0 GME

---
+++ src/test/java/org/obd/metrics/codec/giulia_2_0_gme/UniAirElectovalveTempTest.java
@@ -0,0 +1,39 @@
+/** 
+ * Copyright 2019-2023, Tomasz Żebrowski
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+package org.obd.metrics.codec.giulia_2_0_gme;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class UniAirElectovalveTempTest implements Giulia_2_0_GME_Test {
+
+	@ParameterizedTest
+	@CsvSource(value = {
+			"62198F0000=0",
+			"62198F00A0=20",
+			"62198F00D0=26",
+			"62198F00FF=31",
+			"62198F01FF=63",
+			"62198F02FF=95",
+			"62198F03FF=127"
+			}, delimiter = '=')
+	public void parameterizedTest(String input, Integer expected) {
+		assertEquals(input, expected);
+	}
+}
