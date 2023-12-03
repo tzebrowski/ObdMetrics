09daa46543b6562c7f7f4e4ef02a5965d6101fc9
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2023-12-03 20:35:57 +0100

    fix: Convert Engine Speed PID to Int for 2.0 GME

---
+++ src/test/java/org/obd/metrics/codec/giulia_2_0_gme/EngineSpeedTest.java
@@ -0,0 +1,38 @@
+/** 
+ * Copyright 2019-2023, Tomasz Żebrowski
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+package org.obd.metrics.codec.giulia_2_0_gme;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class EngineSpeedTest implements Giulia_2_0_GME_Test {
+
+	@ParameterizedTest
+	@CsvSource(value = { 
+			"6210000000=0",
+			"62100000A0=40",
+			"6210000A00=640",
+			"6210001CC0=1840",
+			"6210007AAA=7850",
+			"621000A000=10240",
+			}, delimiter = '=')
+	public void parameterizedTest(String input, Integer expected) {
+		assertEquals(input, expected);
+	}
+}
