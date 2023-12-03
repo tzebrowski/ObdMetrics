de2b4f97397b23d3c019f729aa11392f61bfbd40
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2023-12-03 20:46:38 +0100

    feat: Add Lambda Intake Temp PID for 2.0 GME

---
+++ src/test/java/org/obd/metrics/codec/giulia_2_0_gme/LambdaIntakeTempTest.java
@@ -0,0 +1,38 @@
+/** 
+ * Copyright 2019-2023, Tomasz Żebrowski
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+package org.obd.metrics.codec.giulia_2_0_gme;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class LambdaIntakeTempTest implements Giulia_2_0_GME_Test {
+
+	@ParameterizedTest
+	@CsvSource(value = { 
+			"623A530000=0",
+			"623A530010=16",
+			"623A5300A0=160",
+			"623A5300C0=192",
+			"623A5300D0=208",
+			"623A5301E0=480"
+			}, delimiter = '=')
+	public void parameterizedTest(String input, Integer expected) {
+		assertEquals(input, expected);
+	}
+}
