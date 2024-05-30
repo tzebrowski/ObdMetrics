133b8dafef2cbe1747a9c1bb3fb699e2faf75a0c
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2024-05-30 09:35:24 +0200

    feat: More Alfa 1.75 TBI related PIDs

---
+++ src/test/java/org/obd/metrics/codec/giulietta_qv_med17_3_1/AmbientTempTest.java
@@ -0,0 +1,36 @@
+/** 
+ * Copyright 2019-2024, Tomasz Żebrowski
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+package org.obd.metrics.codec.giulietta_qv_med17_3_1;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class AmbientTempTest implements Giulietta_QV_Med_17_3_1_Test {
+
+	@ParameterizedTest
+	@CsvSource(value = { 
+			"62100500=-40",
+			"621005AA=130",
+			"621005FF=215"
+
+	}, delimiter = '=')
+	public void parameterizedTest(String input, Integer expected) {
+		assertEquals(input, expected);
+	}
+}
