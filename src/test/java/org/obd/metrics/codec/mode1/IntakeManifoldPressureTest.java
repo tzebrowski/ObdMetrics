adc16e65563f6bb175dcfedd6674a36802754208
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2026-01-05 18:02:34 +0100

    feat: update files header

--- src/test/java/org/obd/metrics/codec/mode1/IntakeManifoldPressureTest.java
+++ src/test/java/org/obd/metrics/codec/mode1/IntakeManifoldPressureTest.java
@@ -1,5 +1,5 @@
  /**
- * Copyright 2019-2025, Tomasz Żebrowski
+ * Copyright 2019-2026, Tomasz Żebrowski
  *
  * <p>Licensed to the Apache Software Foundation (ASF) under one or more contributor license
  * agreements. See the NOTICE file distributed with this work for additional information regarding
