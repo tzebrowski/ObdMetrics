75bac2d9512d42344b9522710646c07c14b91ea7
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2026-02-22 20:40:56 +0100

    fix: fix failing tests

--- src/test/java/org/obd/metrics/codec/batch/Med17_3_STN_BatchCodecTest.java
+++ src/test/java/org/obd/metrics/codec/batch/Med17_3_STN_BatchCodecTest.java
@@ -21,8 +21,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.obd.metrics.api.model.Adjustments;
 import org.obd.metrics.api.model.BatchPolicy;
 import org.obd.metrics.api.model.STNxxExtensions;
@@ -39,18 +37,8 @@ public class Med17_3_STN_BatchCodecTest extends BatchCodecTestRunner {
 					  .enabled(Boolean.TRUE).build())
 			  .build();
 	
-	final Adjustments ADJUSTEMENTS_NUMBER_OF_FRAMES = Adjustments
-			.builder()
-			.stNxx(STNxxExtensions.builder()
-					.enabled(Boolean.TRUE)
-					.promoteSlowGroupsEnabled(Boolean.FALSE).build())
-			  .batchPolicy(BatchPolicy.builder()
-					  .calculateResponseFrames(true)
-					  .enabled(Boolean.TRUE).build())
-			  .build();
-	
 	@Test
-	public void case_12_pids(boolean numberOfFramesEnabled) {
+	public void case_12_pids() {
 		final Map<Object, Object> expectedValues = new HashMap<>();
 		expectedValues.put("1000", 0);
 		expectedValues.put("1924", 0.0);
