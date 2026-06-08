5c05ef8f81b3e11427e59ab788132beebf9bb575
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2026-06-08 18:33:21 +0200

    feat: test update

--- src/test/java/org/obd/metrics/api/CANModeOverrideTest.java
+++ src/test/java/org/obd/metrics/api/CANModeOverrideTest.java
@@ -232,10 +232,8 @@ public class CANModeOverrideTest {
 		        .requestResponse("010B", "410b35")
 		        .requestResponse("2204FE", "6204FE4E")
 		        .requestResponse("22051A", "62051A11")
-
 		        .requestResponse("221937", "621937011C")
 		        .requestResponse("222181F", "62181F0119")
-
 		        .build();
 		
 		final Init init = Init.builder()
@@ -245,7 +243,6 @@ public class CANModeOverrideTest {
 				//overrides CAN mode
 				.header(Header.builder().mode("555").header("DA18F1").build())
 		        .protocol(Protocol.CAN_29)
-		        
 		        .sequence(DefaultCommandGroup.INIT).build();
 			
 		final Adjustments optional = Adjustments
